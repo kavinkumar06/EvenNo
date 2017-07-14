0271eb86cb6f85ec4f936454f6b4d708f7dc1a7c
kavinkumar06 <kavinm09@gmail.com>
2017-07-14 10:22:06 +0530

    Create EvenNo.java

---
+++ EvenNo.java
@@ -0,0 +1,18 @@
+# EvenNo
+import java.io.*;
+import java.util.*;
+public class EvenNo
+{
+  public static void main(String args[])
+  {
+    int limit=50;
+    System.out.println("Printing even numbers between 1 and "+limit);
+    for(int i=1;i<=limit;i++)
+    {
+    if(i%2==0)
+    {
+     System.out.println(i+" ");
+     }
+    }
+   }
+  } 
